fc5b582779f8abbd9717e3fd53d9c26bdd0fc32f
Gich-M <gichmuriuki21@gmail.com>
2024-10-26 15:58:36 +0300

    div

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,6 +1,16 @@
 -- Creates a function SafeDiv that safely divides two numbers
 -- Returns 0 if the denominator is 0
 
-CREATE OR REPLACE FUNCTION SafeDiv(a INT, b INT)
-RETURNS INT
-RETURN IF(b = 0, 0, a / b);
+DROP FUNCTION IF EXISTS `safeDiv`;
+DELIMITER $$
+
+CREATE OR REPLACE FUNCTION SafeDiv(a INT, b INT) 
+RETURNS DECIMAL(65,20)
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+    RETURN a / b;
+END $$
+
+DELIMITER ;
