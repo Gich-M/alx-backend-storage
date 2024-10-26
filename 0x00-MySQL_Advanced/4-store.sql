4ecad22df06375a681868d9de953b27e4ace4f08
Gich-M <gichmuriuki21@gmail.com>
2024-10-26 14:40:43 +0300

    store

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,5 +1,5 @@
 -- Creates a trigger that decreases the quantity of an item after adding a new order
-DROP TRIGGER IF EXISTS reduced_quantity;
+DROP TRIGGER IF EXISTS reduce_quantity;
 DELIMITER $$
 CREATE TRIGGER reduce_quantity
 AFTER INSERT ON orders
