1c40abf680e5f4efbf8f9605dca4f50b675ac221
Gich-M <gichmuriuki21@gmail.com>
2024-11-03 16:49:45 +0300

    web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -21,11 +21,13 @@ def data_cacher(method: Callable) -> Callable:
         """
         redis_store.incr(f"count:{url}")
         result = redis_store.get(f"result:{url}")
+        
         if result:
             return result.decode('utf-8')
+
         result = method(url)
-        redis_store.set(f"count:{url}", 0)
         redis_store.setex(f"result:{url}", 10, result)
+
         return result
     return wrapper
 
