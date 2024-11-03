3f60c2a31f73eacd8d066a0a7d60568b2d6cf4ff
Gich-M <gichmuriuki21@gmail.com>
2024-11-03 16:15:46 +0300

    Decorate (Cache.store) - call_history

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -25,12 +25,39 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    Decorator to store the history of inputs
+        and outputs for a function using
+        Redis lists to store the history.
+
+    Args:
+        method: The method to be decorated
+
+    Return:
+        Callable: The wrapped method that stores
+            input/output history
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input_list_key = f"{method.__qualname__}:inputs"
+        output_list_key = f"{method.__qualname__}:outputs"
+
+        self._redis.rpush(input_list_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_list_key, str(output))
+
+        return output
+    return wrapper
+
+
 class Cache:
     def __init__(self) -> None:
         """Initialize Redis client and flush the database"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
